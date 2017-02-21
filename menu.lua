487d00560a3a1fd22903155d0f10593067088a3c
brent-corona <brent@coronalabs.com>
2017-02-21 12:17:10 -1000

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -27,26 +27,25 @@ end
 -- create()
 function scene:create( event )
 
-    local sceneGroup = self.view
-    -- Code here runs when the scene is first created but has not yet appeared on screen
+	local sceneGroup = self.view
+	-- Code here runs when the scene is first created but has not yet appeared on screen
 
-    local background = display.newImageRect( sceneGroup, "background.png", 800, 1400 )
-    background.x = display.contentCenterX
-    background.y = display.contentCenterY
+	local background = display.newImageRect( sceneGroup, "background.png", 800, 1400 )
+	background.x = display.contentCenterX
+	background.y = display.contentCenterY
 
-    local title = display.newImageRect( sceneGroup, "title.png", 500, 80 )
-    title.x = display.contentCenterX
-    title.y = 200
+	local title = display.newImageRect( sceneGroup, "title.png", 500, 80 )
+	title.x = display.contentCenterX
+	title.y = 200
 
-    local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, 700, native.systemFont, 44 )
-    playButton:setFillColor( 0.82, 0.86, 1 )
+	local playButton = display.newText( sceneGroup, "Play", display.contentCenterX, 700, native.systemFont, 44 )
+	playButton:setFillColor( 0.82, 0.86, 1 )
 
-    local highScoresButton = display.newText( sceneGroup, "High Scores", display.contentCenterX, 810, native.systemFont, 44 )
-    highScoresButton:setFillColor( 0.75, 0.78, 1 )
+	local highScoresButton = display.newText( sceneGroup, "High Scores", display.contentCenterX, 810, native.systemFont, 44 )
+	highScoresButton:setFillColor( 0.75, 0.78, 1 )
 
-    playButton:addEventListener( "tap", gotoGame )
-    highScoresButton:addEventListener( "tap", gotoHighScores )
-
+	playButton:addEventListener( "tap", gotoGame )
+	highScoresButton:addEventListener( "tap", gotoHighScores )
 end
 
 
@@ -87,7 +86,6 @@ function scene:destroy( event )
 
 	local sceneGroup = self.view
 	-- Code here runs prior to the removal of scene's view
-
 end
 
 
